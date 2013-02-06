22ddb7994c1d01a7389917f8bff20d0f8566be2f
Vladimir Li <lee.vlad@gmail.com>
2013-02-05 20:24:47 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheNotist::Application.config.secret_token = 'e635b06f992ebd1eda2c579f77cf680c7acd4b52cefe092886ca10b5396a479811f1f4db56be14c25f6fee242ec71daf78983b1344d3902b537ab7bfc2f7a5bb'
