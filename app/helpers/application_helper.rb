b7ca08a0f82c6c00e5095ec0421cacb76ecee28a
Josh Zivot <joshz@fb.com>
2013-02-12 22:27:26 -0800

    Created sidebar_link helper function to create sidebar items with active state based on current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def sidebar_link(text, path, icon_name)
+    class_name = current_page?(path) ? 'active' : ''
+
+    content_tag :li, :class => class_name do
+      link_to path do
+        content_tag(:i, :class => icon_name) do
+        end +
+        content_tag(:span, :class => 'text') do
+          text
+        end
+      end
+    end
+  end
+
 end
