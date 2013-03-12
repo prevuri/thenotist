acaaadfd0dfd2ea66a11ae37acbd2168322999f3
Piyush Revuri <prevuri14@gmail.com>
2013-03-12 01:06:44 -0700

    Completed Facebook Login.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308172105) do
+ActiveRecord::Schema.define(:version => 20130312062650) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20130308172105) do
     t.datetime "updated_at",                             :null => false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
