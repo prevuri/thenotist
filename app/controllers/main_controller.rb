25dc80188e142057f94185508a690f4f24e9b650
joshz <joshz@fb.com>
2013-04-10 22:36:56 -0700

    Server-side controller changes for splash page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
   def index
+    if !current_user
+      render 'splash', :layout => 'minimal'
+    else
+      # Default action (render index)
+    end
   end
 end
