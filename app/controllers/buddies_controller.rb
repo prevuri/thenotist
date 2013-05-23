460c3f1fbfedea44ec002fdc3618ced7cda5d1d7
Piyush Revuri <prevuri14@gmail.com>
2013-05-23 18:38:01 -0400

    Check for current friends

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -2,12 +2,24 @@ class BuddiesController < ApplicationController
   before_filter :authenticate_user!
   def index
   	@graph = Koala::Facebook::API.new(current_user.fb_access_token)
-  	@friend_data = @graph.get_connections("me", "friends?fields=id,name,picture")
-  	# @friends_using
-  	# User.find(:all, :select => :uid).each do |user_id|
-  	# 	@friends_data.each do |friend|
-  			
-  	# end
+  	@friends_data = @graph.get_connections("me", "friends?fields=id,name,picture")
+  	
+  	@friends_using = []
+  	index_hash = Hash[@friends_data.map.with_index.to_a]
+  	
+  	@removed_friends = []
+  	User.all.each do |user|
+  		@friends_data.each do |friend|
+  			if friend["id"] == user.uid
+  				@friends_using << friend
+  				@removed_friends << index_hash[friend]
+  			end
+  		end
+  	end
+
+  	@friends_using.each do |using_friend|
+  		@friends_data.delete_at(@removed_friends.pop)
+  	end
   	# puts @friend_pictures
   end
 end
