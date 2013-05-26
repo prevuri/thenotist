241eb4f4d1ead28b4f171a6426fadd374d305b7b
Matthew Lam <matt.w.lam@gmail.com>
2013-05-25 21:29:45 -0400

    added icon and updated ui theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
 
+  def sidebar_logo_link
+    path = '/main/index'
+    default_class = 'btn btn-large btn-block notist-logo'
+    class_name = default_class + (current_page?(path) ? ' active' : '')
+
+    content_tag :li, :class do
+      link_to path, :class => class_name do
+      end
+    end
+  end
+
   def sidebar_link(text, path, icon_name)
     default_class = 'btn btn-large btn-block'
     class_name = default_class + (current_page?(path) ? ' active' : '')
@@ -14,5 +25,4 @@ module ApplicationHelper
       end
     end
   end
-
 end
