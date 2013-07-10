4b7f0e120d0e0f948bff8cf2a2384becd976100f
Piyush Revuri <prevuri14@gmail.com>
2013-07-10 17:59:14 -0400

    Changing activities to add followers

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,27 @@ class MainController < ApplicationController
       render 'splash', :layout => 'minimal'
     else
       # Default action (render index)
-      unless current_user.buddies.empty?
-        @activities = current_user.buddy_activities
-        @activities = @activities.order("created_at DESC")
+    
+      follow = current_user.followers.select { |f| !current_user.buddies.include? f }
+      follow = follow.flatten
+
+      buddy_act = current_user.buddy_activities
+
+      if follow.empty?
+        @activities = buddy_act
+      else
+        follow_act = []
+        follow.each do |follower|
+          follow_act << follower.activities.select { |a| (a.trackable_type == "Relationship" and a.trackable.buddy == current_user) }
+        end
+        follow_act.flatten!
+        puts follow_act.last.user.name
+        @activities = buddy_act + follow_act  
+
+      end
+
+      unless @activities.empty?
+        @activities = @activities.sort_by { |act| act.created_at }
       end
     end
   end
