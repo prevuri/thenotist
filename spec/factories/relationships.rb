b3d6c0a16219738a434ad2bae6af740d504d82c9
Piyush Revuri <prevuri14@gmail.com>
2014-01-08 19:46:51 -0500

    Integration Tests Added; Cucumber Tests Removed

--- spec/factories/relationships.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :relationship do
-    user_id 1
-    buddy_id 1
-    blocked false
-  end
-end
