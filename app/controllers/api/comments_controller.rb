860c1b4e4bba4eb09663959511ddf2dc4419a675
Piyush Revuri <prevuri14@gmail.com>
2014-03-11 17:13:57 -0400

    Fixed the api call to make one db request

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,25 +2,26 @@ class Api::CommentsController < ApplicationController
   include ApiHelper
 
   before_filter :check_authenticated_user!
-  before_filter :get_uploaded_html_file_id, :only => [ :index, :create ]
+  before_filter :get_uploaded_html_file_id, :only => [ :create ]
+  before_filter :get_note_id, :only => [:index]
   before_filter :get_comment_id, :only => :destroy
 
   def index
     begin
-      @file = current_user.uploaded_html_files.find(@uploaded_html_file_id) # throws an exception if nothing found
+      @note = current_user.notes.find(@note_id) # throws an exception if nothing found
     rescue
       begin
-        @file = current_user.shared_uploaded_html_files.find(@uploaded_html_file_id) # throws an exception if nothing found
+        @note = current_user.shared_notes.find(@note_id) # throws an exception if nothing found
       rescue
         return render :json => {
           :success => false,
-          :error => file_not_found_error
+          :error => note_not_found_error
         }
       end
     end
 
     # only render top-level comments, because their children will be rendered recursively
-    @comments = @file.top_level_comments
+    @comments = @note.top_level_comments
     return render :json => {
       :success => true,
       :comments => @comments.map { |c| c.as_json(:current_user => current_user) }
@@ -95,7 +96,34 @@ class Api::CommentsController < ApplicationController
     }
   end
 
+  def filecomments
+    begin
+      @file = current_user.uploaded_html_files.find(@uploaded_html_file_id) # throws an exception if nothing found
+    rescue
+      begin
+        @file = current_user.shared_uploaded_html_files.find(@uploaded_html_file_id) # throws an exception if nothing found
+      rescue
+        return render :json => {
+          :success => false,
+          :error => file_not_found_error
+        }
+      end
+    end
+
+    # only render top-level comments, because their children will be rendered recursively
+    @comments = @file.top_level_comments
+    return render :json => {
+      :success => true,
+      :comments => @comments.map { |c| c.as_json(:current_user => current_user) }
+    }
+  end
+
 private
+
+  def get_note_id
+    @note_id = params[:note_id]
+  end
+
   def get_uploaded_html_file_id
     @uploaded_html_file_id = params[:file_id]
   end
