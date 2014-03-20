f415bf1617ca010f65bbc6f5eb292a97d7af5e4a
Vladimir Li <lee.vlad@gmail.com>
2014-03-20 14:26:46 -0400

    Styled the grouped activities.

--- app/presenters/activity_presenter.rb
+++ app/presenters/activity_presenter.rb
@@ -12,7 +12,7 @@ class ActivityPresenter < SimpleDelegator
       if activity.user == current_user
         raw("<div class='activity-header-content-container'>") + raw("You ") + render_partial + render_time_partial + raw("</div>") + render_subsection_partial
       else
-        raw("<div class='activity-header-content-container'>") + raw("<a href='/profile/#{activity.user.id}' class='user-name'>") + activity.user.name.split[0] + raw("</a>") + " " + render_partial + render_time_partial + raw("</div>") + render_subsection_partial
+        raw("<div class='activity-header-content-container'>") + raw("<a href='/profile/#{activity.user.id}' class='user-name'>") + activity.user.name + raw("</a>") + " " + render_partial + render_time_partial + raw("</div>") + render_subsection_partial
       end
     end
   end
