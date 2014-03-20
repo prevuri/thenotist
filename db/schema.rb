c8af359ea137bc3858e5f2c893a249fe43fff658
Josh Zivot <joshzivot@gmail.com>
2014-03-19 21:44:43 -0400

    Removed admin CSS affecting other pages

--- db/schema.rb
+++ db/schema.rb
@@ -190,6 +190,5 @@ ActiveRecord::Schema.define(:version => 20140318181858) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["uid"], :name => "index_users_on_uid"
 
 end
